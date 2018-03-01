4cd09dd1b9f73e72c3e17b7ae20aed754a4af496
vhphung <vhphung@ucsd.edu>
2018-03-01 00:32:13 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	
+	var ucsd_ltlng = {lat: 32.8788033,lng: -117.2364593};
+	
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+	
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
